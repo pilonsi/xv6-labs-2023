0ad8dc59d64fd7239c82d5056997dd9b70576f75
Jorge Fabregat López <jfabregat@protonmail.com>
2024-08-09 11:20:18 +0200

    Added vmprint()

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,31 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprintwalk(pagetable_t pagetable, int depth)
+{
+	depth++;
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+		if (pte & PTE_V) {
+      uint64 child = PTE2PA(pte);
+			for (int d = 0; d < depth; d++) printf(" ..");
+			printf("%d: pte %p pa %p\n", i, pte, child);
+
+			if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+				// this PTE points to a lower-level page table.
+				vmprintwalk((pagetable_t)child, depth);
+			}
+		}
+  }
+}
+
+// prints the contents of the provided pagetable as well as all other
+// pagetables hanging off it.
+void
+vmprint(pagetable_t pagetable)
+{
+	printf("page table %p\n", pagetable);		
+	vmprintwalk(pagetable, 0);
+}
